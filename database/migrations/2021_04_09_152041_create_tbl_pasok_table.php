8b9fdcc26fe20fcc349f70a33e0acad5027773ce
marchella2 <marchella.putris2@gmail.com>
2021-04-09 23:06:00 +0700

    Create table

---
+++ database/migrations/2021_04_09_152041_create_tbl_pasok_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblPasokTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_pasok', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_distributor');
+            $table->bigInteger('id_buku');
+            $table->integer('jumlah');
+            $table->date('tanggal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_pasok');
+    }
+}
---
+++ database/migrations/2021_04_09_160118_create_tbl_setting_lap_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblSettingLapTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_setting_lap', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_perusahaan');
+            $table->string('alamat');
+            $table->string('no_tlpn');
+            $table->string('web');
+            $table->string('logo');
+            $table->string('no_hp');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_setting_lap');
+    }
+}
